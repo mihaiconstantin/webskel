843e8df64d8d266e7b635a9e7e589024569dd5e7
mihaiconstantin <c.tinmihai@ymail.com>
2019-02-28 23:21:18 +0100

    Updated `blog.js` entry to use the newly created components and the `Vuex` store

--- resources/js/blog.js
+++ resources/js/blog.js
@@ -17,13 +17,21 @@ Vue.use(VueAos, { duration: 500, offset: 300, easing: 'ease-out-back' });
 import { config } from './config.js';
 
 
+// Import Vuex store.
+import { store } from './store/store.js';
+
+
 // Import the Vue components.
 import Navigation from './components/Navigation.vue';
+import BlogHeader from './components/BlogHeader.vue';
+import PostList from './components/PostList.vue';
 import Footer from './components/Footer.vue';
 
 
 // Globally register the Vue components.
 Vue.component('app-navigation', Navigation);
+Vue.component('app-blog-header', BlogHeader);
+Vue.component('app-post-list', PostList);
 Vue.component('app-footer', Footer);
 
 
@@ -32,6 +40,13 @@ Vue.config.productionTip = false;
 
 const app = new Vue({
     el: '#app',
+    store: store,
+    
+
+    beforeCreate() {
+        this.$store.dispatch('fetchPosts', { api: `${ document.getElementById("app").getAttribute('data-app-api').replace('blog', 'posts') }` });
+    },
+
 
     mounted() {
         this.$nextTick(() => {
