d2f06b01f9a471fcaf7d039dd53cdff3c37b40e2
mihaiconstantin <c.tinmihai@ymail.com>
2019-03-03 17:06:19 +0100

    Added `post` method to the `BlogController`

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Navigation;
+use App\Models\Post;
 
 class BlogController extends Controller
 {
@@ -23,12 +24,17 @@ class BlogController extends Controller
     // Post page of the blog.
     public function post($slug) 
     {
+        $post = Post::where('slug', '=', '$slug')->firstOrFail();
+        $post->next = Post::pickNext($post->category->id, $post->slug);
+
         $data = array(
             'links' => Navigation::linksForContext('blog.index'),
+            'post' => $post
         );
         
         $settings = $this->prepareSettings('blog.index');        
-        
+
         return view('blog.post', $data, $settings);
     }
 }
+
