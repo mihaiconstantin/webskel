b600ab68602daeb7fbebf58448f8033ff9785866
mihaiconstantin <c.tinmihai@ymail.com>
2019-03-03 10:39:44 +0100

    Added `v-if` to hide blog footer until all posts have been fetched

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -38,7 +38,7 @@
 
                     <app-post-list></app-post-list>
 
-                    <app-footer twitter-icon="{{ $twitter }}" home-url="{{ route('home.index') }}" copyright="{{ $copyright }}"></app-footer>
+                    <app-footer v-if="ready" twitter-icon="{{ $twitter }}" home-url="{{ route('home.index') }}" copyright="{{ $copyright }}"></app-footer>
 
                 </main>
             </div>
